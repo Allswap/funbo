eba6010341e51ca808faeb5cf46552b66c5afc42
DiFi Era <nobtx.io@gmail.com>
2026-05-31 01:55:42 +0300

    updat funbo

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -1,4 +1,4 @@
-const WORKER_URL = 'https://funbo-9cr.pages.dev';
+const WORKER_URL = 'funbo.nobtx-io.workers.dev';
 
 export async function onRequest(context: { request: Request; env: Record<string, string> }) {
   const url = new URL(context.request.url);
